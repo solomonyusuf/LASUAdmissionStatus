29e80ed5759711f6263a143281beaeff337473bd
Solomon yusuf <olafire03@gmail.com>
2024-03-16 03:09:01 +0100

    refactor: change codebase build plugin

---
+++ src/main/java/com/admission/lasuadmissionstatus/LasuAdmissionStatusApplication.java
@@ -0,0 +1,13 @@
+package com.admission.lasuadmissionstatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LasuAdmissionStatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LasuAdmissionStatusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/admission/lasuadmissionstatus/LasuAdmissionStatusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.admission.lasuadmissionstatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LasuAdmissionStatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
